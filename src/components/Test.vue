f5038c49b1f8343cc9809bdb7d59aca4c971d1af
KJJH <kjjh@KJJHui-MacBook.local>
2018-01-03 11:34:18 +0900

    init dev

---
+++ src/components/Test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    Test
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Test',
+  data() {
+    return {
+      msg: 'dooboo',
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="postcss" scoped>
+:root {
+  --fontSize: 1em;
+}
+
+@custom-media --mobile (width <= 768px);
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  @media (--mobile) {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,15 @@ import Router from "vue-router";
 Vue.use(Router);
 
 import Main from "@/components/Main.vue";
+import Test from "@/components/Test.vue";
 
 export default new Router({
   routes: [
+    {
+      path: "/test",
+      name: "test",
+      component: Test,
+    },
     {
       path: "/",
       name: "main",
