830ab9fbf9bb7a65300946f117c7ac5d463e3173
KJJH <kjjh@KJJHui-MacBook.local>
2018-01-11 14:53:02 +0900

    Deleted TestLogin.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,20 +4,14 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 import Main from '@/components/screen/Main.vue';
-import Test from '@/components/screen/Login.vue';
-import TestLogin from '@/components/screen/TestLogin.vue';
+import Login from '@/components/screen/Login.vue';
 
 export default new Router({
   routes: [
     {
-      path: '/testlogin',
-      name: 'testlogin',
-      component: TestLogin,
-    },
-    {
       path: '/login',
       name: 'login',
-      component: Test,
+      component: Login,
     },
     {
       path: '/',
